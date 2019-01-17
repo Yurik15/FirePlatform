006d1258a37483bcb359b36e5d3689b3387f6ad8
Yurii Maznyk <ymaznyk@gmail.com>
2019-01-17 23:44:04 +0100

    web api test + xamarin shopping card part1

---
+++ FirePlatform.Mobile/FirePlatform.Mobile/PageModels/BasketPageModel.cs
@@ -0,0 +1,77 @@
+﻿using FirePlatform.Mobile.Common.Interfaces.Communication;
+using FirePlatform.Mobile.Common.Models;
+using Refit;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirePlatform.Mobile.PageModels
+{
+    public class BasketPageModel : BasePageModel
+    {
+        #region fields
+
+        private TemplateModel _selectedForm;
+
+        #endregion
+
+        #region prop
+        private ObservableCollection<TemplateModel> _ReportedForms;
+        public ObservableCollection<TemplateModel> ReportedForms
+        {
+            get => _ReportedForms;
+            set
+            {
+                _ReportedForms = value;
+                RaisePropertyChanged(nameof(ReportedForms));
+            }
+        }
+        public TemplateModel SelectedForm
+        {
+            get { return _selectedForm; }
+            set
+            {
+                _selectedForm = value;
+                CoreMethods.PushPageModel<HomePageModel>();
+            }
+        }
+
+        #endregion
+
+        #region ctor
+
+        public BasketPageModel()
+        {
+            RefreshData();
+        }
+
+        #endregion
+
+
+        #region Navigation
+
+        private void RefreshData()
+        {
+            try
+            {
+                IsBusy = true;
+                Task.Run(async () =>
+                {
+                    var apiResponse = RestService.For<ITemplateModelApi>(RestApiServerUri);
+                    var container = await apiResponse.GetTemplateModels();
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        ReportedForms = new ObservableCollection<TemplateModel>(container?.DataCollection);
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ FirePlatform.Mobile/FirePlatform.WebApi/FirePlatform.WebApi/Model/Responses/FormTreeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirePlatform.WebApi.Model.Responses
+{
+    public class FormTreeResponse
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
