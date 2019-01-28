8a94ed3e5f8e8fc782aeb6d1de553e3e2de0fda5
Yurii Maznyk <ymaznyk@gmail.com>
2019-01-28 23:37:56 +0100

    test Items controller - TO REMOVE IN THE FUTURE

---
+++ FirePlatform.Mobile/FirePlatform.WebApi/FirePlatform.Models/Enums/ItemInputTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace FirePlatform.Models.Enums
+{
+    public enum ItemInputTypes
+    {
+        Text = 1,
+        Number = 2,
+        Condition = 3,
+        DropDown = 4,
+        MultipleDropDown = 5,
+        Radio = 6,
+        ToolTip = 7
+    }
+}
