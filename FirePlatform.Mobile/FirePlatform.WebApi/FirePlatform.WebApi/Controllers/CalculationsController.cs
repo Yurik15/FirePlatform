f11d1f5a207fe92a5183ca828375eb14804380fa
Yurii Maznyk <ymaznyk@gmail.com>
2019-01-29 00:07:28 +0100

    WebApi - some refactor

--- FirePlatform.Mobile/FirePlatform.WebApi/FirePlatform.WebApi/Controllers/CalculationsController.cs
+++ FirePlatform.Mobile/FirePlatform.WebApi/FirePlatform.WebApi/Controllers/CalculationsController.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
-using FirePlatform.Models.Containers;
 using FirePlatform.Services;
 using FirePlatform.WebApi.Model;
-using FirePlatform.WebApi.Model.Responses;
 using FirePlatform.WebApi.Services;
 using FirePlatform.WebApi.Services.Parser;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirePlatform.WebApi.Controllers
 {
     [ApiController]
-    public class CalculationController : BaseController
+    public class CalculationsController : BaseController
     {
         public static List<_ItemGroup> UsersTmp { get; set; }
 
         readonly ICalculationService _calculationService;
-        public CalculationController(Service service, IMapper mapper, ICalculationService calculationService)
+        public CalculationsController(Service service, IMapper mapper, ICalculationService calculationService)
                               : base(service, mapper)
         {
             _calculationService = calculationService;
         }
-        [HttpGet("api/[controller]/loadTmp")]
-        [ProducesResponseType(200, Type = typeof(FormResponse))]
+        [HttpGet("api/[controller]/LoadTmp")]
+        [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<ApiContainer<string>>> Load(int numberTmpl = 1)
+        [EnableCors("AllowAll")]
+        public OkObjectResult Load(int numberTmpl = 1)
         {
             var content = Download(numberTmpl);
 
@@ -38,18 +37,19 @@ namespace FirePlatform.WebApi.Controllers
             return Ok(res);
         }
 
-        [HttpGet("api/[controller]/set")]
-        [ProducesResponseType(200, Type = typeof(FormResponse))]
+        [HttpGet("api/[controller]/Set")]
+        [ProducesResponseType(200)]
         [ProducesResponseType(404)]
         [ProducesResponseType(400)]
-        public async Task<ActionResult<ApiContainer<string>>> Set(int groupId, int itemId, string value)
+        [EnableCors("AllowAll")]
+        public OkObjectResult Set(int groupId, int itemId, string value)
         {
             var group = UsersTmp.FirstOrDefault(x => x.IndexGroup == groupId);
             var item = group.Items.FirstOrDefault(x => x.NumID == itemId);
             dynamic newValue = null;
             if (item.Type == ItemType.Num.ToString())
             {
-                newValue = Double.Parse(value);
+                newValue = double.Parse(value);
             }
             else if (item.Type == ItemType.Check.ToString())
             {
@@ -71,7 +71,6 @@ namespace FirePlatform.WebApi.Controllers
             using (var wc = new System.Net.WebClient())
             {
                 wc.Encoding = System.Text.Encoding.UTF8;// GetEncoding("Windows-1250"); 
-
 
                 if (numberTmpl == 1) file_contents = wc.DownloadString("https://onedrive.live.com/download.aspx?cid=9214918BD14C3E0C&resid=9214918BD14C3E0C%21771&authkey=ANErohHGOIz32s0");
                 if (numberTmpl == 2) file_contents = wc.DownloadString("https://docs.google.com/spreadsheets/u/1/d/1sPt8y5Qi8DyD2kcwr2hi7HdcmYGd0j41wzXSGPgOw-A/export?format=tsv&id=1sPt8y5Qi8DyD2kcwr2hi7HdcmYGd0j41wzXSGPgOw-A&gid=1727084202");
