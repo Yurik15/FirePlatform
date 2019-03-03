781fbcb5d2d8033edd47f20fe07739ffd562616c
Yurii Maznyk <ymaznyk@gmail.com>
2019-03-03 18:57:39 +0100

    delete dont needed files

--- FirePlatform.Mobile/FirePlatform.Web/src/config/dev.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  API_ENDPOINT: '"https://localhost:44358/api"'
-}
--- FirePlatform.Mobile/FirePlatform.Web/src/config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = merge(prodEnv, {
-    NODE_ENV: '"production"',
-    API_ENDPOINT: '"https://localhost:44358/api"'
-  })
